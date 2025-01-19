f94ea248680d7b0979a3f3d7b1253da1f56714dc
rishikaaa23 <rishika.e.prem@gmail.com>
2025-01-19 11:35:30 +0000

    Add files via upload

---
+++ story.py
@@ -0,0 +1,13 @@
+
+
+import random
+
+when=["a long time ago","six years ago","three months ago","10 days ago"]
+who=["a group of friends","a classroom","random people","enemies"]
+names=["lori","jaelyn","sophia","hannah","abigail"]
+residence=["at a big house","in the school building","inside of a hospital","in a small house"]
+happened=["they all got chased","they had to escape","they had a party","they did their homework"]
+went=["they all went home","they stayed at the place","they continued what they were doing"]
+
+print(random.choice(when)+random.choice(who)+random.choice(names)+random.choice(residence)+random.choice(happened)+random.choice(went))
+
